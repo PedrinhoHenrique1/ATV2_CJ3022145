a7f66b77018251659a31fe2c2361b19f584c72fd
Pedro Henrique de Sá Ferreira <f.sa@aluno.ifsp.edu.br>
2024-03-05 15:20:32 -0300

    Atualização 2

--- ATV2_CJ3022145/Program.cs
+++ ATV2_CJ3022145/Program.cs
@@ -94,7 +94,7 @@ float velocidade;
 
 velocidade = (distância / tempo);
 Console.WriteLine("A velocidade em a ocorreu a ação foi de: " + velocidade.ToString());
-*/
+
 //Atividade 6 -  Lista
 Console.WriteLine("Digite seu salário: ");
 float salário = int.Parse(Console.ReadLine());
@@ -118,3 +118,39 @@ numero = (numero * 3 + 1) + (numero * 2 - 1);
 Console.WriteLine("A soma do sucessor do triplo com o antecessor do dobro é: " + numero.ToString());
 
 //Atividade 9 - Lista
+Console.WriteLine("Digite um número que tenha 4 digítos: ");
+float n1 = float.Parse(Console.ReadLine());
+
+//Atividade 10 - Lista
+float investimento = 1000;
+float variação_dia1 = -3.4f;
+float variação_dia2 = 2.8f;
+float variação_dia3 = 14f;
+float variação_dia4 = -8.5f;
+
+float valor_dia1 = investimento * (1 + variação_dia1 / 100);
+float valor_dia2 = valor_dia1 * (1 + variação_dia2 / 100);
+float valor_dia3 = valor_dia2 * (1 + variação_dia3 / 100);
+float valor_atual = valor_dia3 * (1 + variação_dia4 / 100);
+
+Console.WriteLine("O valor atual dos investimentos de Marquesito é R$" + valor_atual.ToString("0.00"));
+
+//Atividade 11 - Lista
+Console.WriteLine("Digite o número desejado: ");
+float num =  float.Parse(Console.ReadLine());
+float quadrado;
+float cubo;
+
+quadrado = (num * num);
+cubo = (num * num * num);
+
+Console.WriteLine("O número ao quadrado é: {0} \n O resultado ao cubo é: {1}", quadrado, cubo);
+*/
+//Atividade 12 - Lista 
+
+
+//Atividade 13 - Lista
+Console.WriteLine("O github é quase como uma rede social dev, utilizado para compartilhar ideias e projetos, podendo até ser utilizado para pegar projetos prontos e apeerfeiçoa-los, já o git é um programa param armazenar arquivos sendo possível ver todas as atualizações mais recentes e também passadas do projetos.");
+
+
+
