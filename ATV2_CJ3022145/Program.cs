e23a7b3b252e9ebb0c763b8c685020937e2d4be7
Pedro Henrique de Sá Ferreira <f.sa@aluno.ifsp.edu.br>
2024-03-06 10:06:27 -0300

    Finalizado

--- ATV2_CJ3022145/Program.cs
+++ ATV2_CJ3022145/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("Atividade 1");
 //Atividade 1 - Sala
-float investimento = 1000;
+float investimento1 = 1000;
 float variação_dia1 = -3.4f;
 float variação_dia2 = 2.8f;
 float variação_dia3 = 14f;
 float variação_dia4 = -8.5f;
 
-float valor_dia1 = investimento * (1 + variação_dia1 / 100);
+float valor_dia1 = investimento1 * (1 + variação_dia1 / 100);
 float valor_dia2 = valor_dia1 * (1 + variação_dia2 / 100);
 float valor_dia3 = valor_dia2 * (1 + variação_dia3 / 100);
 float valor_atual = valor_dia3 * (1 + variação_dia4 / 100);
@@ -82,7 +82,7 @@ Console.WriteLine("Digite a terceira nota: ");
 float nota3 = float.Parse(Console.ReadLine());
 Console.WriteLine("Digite o peso da terceira nota: ");
 float peso3 = float.Parse(Console.ReadLine());
-float total;
+float total2;
 
 total = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
 Console.WriteLine("A média poderada dos pesos é: " + total.ToString());
@@ -99,9 +99,9 @@ Console.WriteLine("A velocidade em a ocorreu a ação foi de: " + velocidade.ToS
 
 //Atividade 6 -  Lista
 Console.WriteLine("Digite seu salário: ");
-float salário = int.Parse(Console.ReadLine());
-float aumento = salário * 0.25f;
-float total = salário + aumento;
+float salário1 = int.Parse(Console.ReadLine());
+float aumento1 = salário * 0.25f;
+float total1 = salário + aumento;
 Console.WriteLine("O salário pós aumento é " + total.ToString("0.00"));
 
 //Atividade 7 - Lista 
@@ -115,25 +115,39 @@ Console.WriteLine("O valor em real convertido para dólar é: " + dolar.ToString
 
 //Atividade 8 - Lista
 Console.WriteLine("Digite o número desejado: ");
-int numero = int.Parse(Console.ReadLine());
+int numero1 = int.Parse(Console.ReadLine());
 numero = (numero * 3 + 1) + (numero * 2 - 1);
 Console.WriteLine("A soma do sucessor do triplo com o antecessor do dobro é: " + numero.ToString());
 
 //Atividade 9 - Lista
+int val, milhar, milhar1, milhar2, centena, centena1, dezena, dezena1, unidade, val1, val2;
+
 Console.WriteLine("Digite um número que tenha 4 digítos: ");
-float n1 = float.Parse(Console.ReadLine());
+val = int.Parse(Console.ReadLine());
+milhar1 = val / 1000;
+milhar2 = milhar1 * 1000;
+centena = (val - milhar1) / 1000;
+centena1 = centena * 100;
+val1 = milhar2 + centena1;
+dezena = (val - val1) / 10;
+dezena1 = dezena * 10;
+val2 = milhar1 + centena1 + dezena1;
+unidade = val - val2;
+
+Console.WriteLine("{0}\n{1}\n{2}\n{3}", milhar1, centena, dezena, unidade);
+
 
 //Atividade 10 - Lista
-float investimento = 1000;
-float variação_dia1 = -3.4f;
-float variação_dia2 = 2.8f;
-float variação_dia3 = 14f;
-float variação_dia4 = -8.5f;
+float investimento = 1000.0f;
+float variação_dia10 = -3.4f;
+float variação_dia20 = 2.8f;
+float variação_dia30 = 14f;
+float variação_dia40 = -8.5f;
 
-float valor_dia1 = investimento * (1 + variação_dia1 / 100);
-float valor_dia2 = valor_dia1 * (1 + variação_dia2 / 100);
-float valor_dia3 = valor_dia2 * (1 + variação_dia3 / 100);
-float valor_atual = valor_dia3 * (1 + variação_dia4 / 100);
+float valor_dia10 = investimento * (1 + variação_dia10 / 100);
+float valor_dia20 = valor_dia1 * (1 + variação_dia2 / 100);
+float valor_dia30 = valor_dia2 * (1 + variação_dia3 / 100);
+float valor_atual0 = valor_dia3 * (1 + variação_dia4 / 100);
 
 Console.WriteLine("O valor atual dos investimentos de Marquesito é R$" + valor_atual.ToString("0.00"));
 
@@ -150,11 +164,16 @@ Console.WriteLine("O número ao quadrado é: {0} \n O resultado ao cubo é: {1}"
 
 //Atividade 12 - Lista
 Console.WriteLine("Digite sua altura: ");
-float altura = float.Parse(Console.ReadLine());
-Console.WriteLine("Digite seu gênero: (Homem ou Mulher).")
-char gen = char.Parse(Console.ReadLine());
+float altura1 = float.Parse(Console.ReadLine());
+double ideal1;
+double ideal2;
 
-if (gen == "Homem")
 
-    //Atividade 13 - Lista
-    Console.WriteLine("O github é quase como uma rede social dev, utilizado para compartilhar ideias e projetos, podendo até ser utilizado para pegar projetos prontos e apeerfeiçoa-los, já o git é um programa param armazenar arquivos sendo possível ver todas as atualizações mais recentes e também passadas do projetos. \n Alguns comandos do git são: git config, git init, git clone, git add, git commit, git branch, git checkout, git remote, git push, git fetch);
+ideal1 = (72.7 * altura - 58);
+ideal2 = (62.1 * altura - 44.7);
+
+Console.WriteLine("Para mulher, o peso ideal é: {0} \n Para homem, o peso ideal é: {1}", ideal2, ideal1);
+
+
+//Atividade 13 - Lista
+Console.WriteLine("O github é quase como uma rede social dev, utilizado para compartilhar ideias e projetos, podendo até ser utilizado para pegar projetos prontos e apeerfeiçoa-los, já o git é um programa param armazenar arquivos sendo possível ver todas as atualizações mais recentes e também passadas do projetos. \n Alguns comandos do git são: git config, git init, git clone, git add, git commit, git branch, git checkout, git remote, git push, git fetch");
